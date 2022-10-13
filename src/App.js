7982b3427a80f45c837a4ec042fca2793931a8ae
amit0961 <amitsaha5bd@gmail.com>
2022-10-13 10:53:47 +0600

    Navabar shows and other page routing setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
+
 function App() {
   return (
     <BrowserRouter>
+      <nav>Our Navbar Here</nav>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<Error />} />
+        {/* <Route
           path="testing"
           element={
             <div>
               <h2>Testing Page</h2>
             </div>
           }
-        />
+        /> */}
       </Routes>
+      <footer>Our Footer Here</footer>
     </BrowserRouter>
   );
 }
