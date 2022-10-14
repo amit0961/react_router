146bf62488438b672271588bd247a9356d8a4310
Amit <amitsaha5bd@gmail.com>
2022-10-15 02:25:30 +0600

    signle page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,14 @@
+import { Link , useParams } from "react-router-dom";
+import products from "../data";
 const SingleProduct = () => {
+  const {productId} = useParams();
+  const product = products.find((product)=>product.id === productId);
+  const {image,name} = product;
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <img src="{image}" alt="{image}" srcSet={`${image} 300w, ${image} 768w, ${image} 1280w`} />
+      <h4>{name}</h4>
+      <Link to='/products'>Back to Products</Link>
     </section>
   );
 };
